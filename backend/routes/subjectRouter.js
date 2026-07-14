d1ff2aeb84422ac6d6bdea3d3ac7732375c9c61b
Rahul Behera <rahulbehera9469@gmail.com>
2026-07-14 22:29:05 +0530

    fixed questions fetching

--- backend/routes/subjectRouter.js
+++ backend/routes/subjectRouter.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+
 const auth = require('../middleware/auth');
 
-const { handleQuestions, handleMock } = require('../controller/subjectController');
+const { 
+    handleQuestions, 
+    handleMock 
+} = require('../controller/subjectController');
 
-router.get("/:subject", auth, handleQuestions);
+
+router.get("/questions/:subject", auth, handleQuestions);
+
 router.get("/mock/:subject", auth, handleMock);
+
 
 module.exports = router;
