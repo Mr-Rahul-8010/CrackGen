1038447deb19d4c8e6efe5a42c18c0a82e13b247
Rahul Behera <rahulbehera9469@gmail.com>
2026-07-14 21:23:18 +0530

    Fixed error 404

--- backend/routes/subjectRouter.js
+++ backend/routes/subjectRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const { handleQuestions, handleMock} = require('../controller/subjectController');
 
-router.get("/questions/:subject", auth, handleQuestions);
+const { handleQuestions, handleMock } = require('../controller/subjectController');
+
+router.get("/:subject", auth, handleQuestions);
 router.get("/mock/:subject", auth, handleMock);
+
 module.exports = router;
