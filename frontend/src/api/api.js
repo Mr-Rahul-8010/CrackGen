986ea7b7a0a97459a453980a101f0cc8c41d5e00
Rahul Behera <rahulbehera9469@gmail.com>
2026-07-14 21:34:28 +0530

    removed protected API call from Home

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,13 @@
 export async function getMessage(){
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/message`);
+
+    const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/message`,
+        {
+            credentials: "include"
+        }
+    );
+
     const data = await response.json();
+
     return data;
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,37 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import { Navigate, useNavigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 import './Home.css';
 
-import { getMessage } from '../api/api';
-
 const Home = () => {
-
-    const [msg,setMsg] = useState(""); // backend 
-
-    useEffect(()=>{
-        getMessage()
-        .then((data)=>{
-            console.log(data);
-            setMsg(data.message);
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-    },[])
 
     const navigate = useNavigate();
 
     return (
         <>
 
-            {/* <h1>{msg}</h1> */}
-
             <Navbar />
+
             <div className='home-container'>
 
                 <div className='home-content'>
+
                     <h1 className='home-title'>
                         Welcome to CrackGen
                     </h1>
@@ -43,6 +28,7 @@ const Home = () => {
                     </p>
 
                     <div className='home-quotes'>
+
                         <p>
                             Crack interviews with confidence.
                             Learn, Practice, and Get Hired.
@@ -51,25 +37,38 @@ const Home = () => {
                         <p>
                             Your journey to becoming interview-ready starts here.
                         </p>
+
                     </div>
 
+
                     <div className='btn-container'>
-                        <button onClick={() => navigate('/signup')} className='join-btn'>
+
+                        <button 
+                            onClick={() => navigate('/signup')} 
+                            className='join-btn'
+                        >
                             Crack Now
                         </button>
 
-                        <button onClick={() => navigate('/subjects')} className='explore-btn'>
+
+                        <button 
+                            onClick={() => navigate('/subjects')} 
+                            className='explore-btn'
+                        >
                             Explore Subjects
                         </button>
+
                     </div>
 
                 </div>
+
             </div>
+
 
             <Footer />
 
         </>
-    )
+    );
 }
 
 export default Home;
